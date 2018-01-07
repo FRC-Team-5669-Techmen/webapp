1eb0b6943380592c215238af60580b79a50d6d70
CodeCube <therealcodecube@gmail.com>
2018-01-06 23:17:10 -0800

    Added part requests browsing page
    Currently none of the buttons work
    But you can see parts requests grouped into different stages of
    completion.

--- src/app/part-requests-page/part-requests-page.component.ts
+++ src/app/part-requests-page/part-requests-page.component.ts
@@ -1,3 +1,4 @@
+import { AccessLevel, PartRequest, WebappBackendService, PartRequestStatus } from '../webapp-backend.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./part-requests-page.component.scss']
 })
 export class PartRequestsPageComponent implements OnInit {
-  constructor() { }
+  requests: Array<PartRequest> = [];
+
+  get AccessLevel() { // For *ngIfs
+    return AccessLevel;
+  }
+
+  get PartRequestStatus() {
+    return PartRequestStatus;
+  }
+
+  constructor(private backend: WebappBackendService) { }
 
   ngOnInit() {
+    this.requests = [];
+    this.backend.getCurrentMemberAsync().then((member) => {
+      this.backend.getPartRequests().then((res) => {
+        if (res.ok) {
+          this.requests = res.body;
+        }
+      });
+    });
+  }
+
+  getAllRequestsWithStatus(status: PartRequestStatus) {
+    return this.requests.filter((e) => e.status === status);
+  }
+
+  getAllRequestsByCurrentMember() {
+    return this.requests.filter((e) => {
+      return (e.requestedBy === this.backend.pollCurrentMember().emailAddress) && (e.status === this.PartRequestStatus.PENDING);
+    });
+  }
+
+  canEdit(request: PartRequest) {
+    const m = this.backend.pollCurrentMember();
+    return ((request.requestedBy === m.emailAddress) || (m.accessLevel === AccessLevel.LEADER))
+        && (request.status === PartRequestStatus.PENDING);
   }
 }
