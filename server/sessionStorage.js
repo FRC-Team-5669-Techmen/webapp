fa6c58a075b69a4cd6d30fd904331f766febf0bc
CodeCube <therealcodecube@gmail.com>
2018-08-17 09:50:11 -0700

    Sessions can now handily retrieve a refreshed Discord access token.
    For use when you need to do something with a user's discord access
    token.

--- server/sessionStorage.js
+++ server/sessionStorage.js
@@ -1,4 +1,5 @@
 const memberDatabase = require('./databases').members;
+const rawDiscord = require('./rawDiscord');
 
 var sessions = [];
 const ACCESS_LEVEL_VISITOR = 'visitor', ACCESS_LEVEL_RESTRICTED = 'restricted', ACCESS_LEVEL_MEMBER = 'member' 
@@ -15,17 +16,37 @@ function generateRandomToken() {
 
 module.exports.createNewSession = function() {
 	let session = {
-			token: generateRandomToken(),
-			memberId: null,
-			started: Date.now(),
-			lastRefresh: Date.now(),
-			get accessLevel() {
-				let member = memberDatabase.findItemWithValue('id', this.memberid);
-				return (member) ? member.level : ACCESS_LEVEL_VISITOR;
-			}
+		token: generateRandomToken(),
+		memberId: null,
+		started: Date.now(),
+		lastRefresh: Date.now(),
+		get accessLevel() {
+			let member = memberDatabase.findItemWithValue('id', this.memberId);
+			return (member) ? member.level : ACCESS_LEVEL_VISITOR;
+		},
+		getDiscordAuthToken: function(redirectHost, callback) {
+			memberDatabase.findItemWithValue('id', this.memberId, (member) => {
+				if (member.connections.discord.accessTokenExp < (Date.now() / 1000 + 60 * 60 * 12)) { // Its 12 hours from expiring, refresh it. (They last a week.)
+					rawDiscord.getNewToken(member.connections.discord.refreshToken, redirectHost, (tokenData) => {
+						member.connections.discord.accessToken = tokenData.access_token;
+						member.connections.discord.accessTokenExp = Date.now() / 1000 + tokenData.expires_in;
+						callback(tokenData.access_token);
+					});
+				} else {
+					callback(member.connections.discord.accessToken);
+				}
+			});
+		}
 	};
 	sessions.push(session);
 	return session;
+}
+
+if (!productionMode) {
+	module.exports.createNewSession();
+	ts = sessions[sessions.length - 1];
+	ts.token = 'abc123def420';
+	ts.memberId = 'OsISBaYSPgBT7fdvBQYCypXocBt2QgpGTdK6CQ1mNihO47jXElhG6Wcvj6gHLiQ1';
 }
 
 module.exports.getSessionByToken = function(token) {
