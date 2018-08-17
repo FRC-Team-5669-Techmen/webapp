16b312dacf72dee6e4382b114cc513afa17834d7
CodeCube <therealcodecube@gmail.com>
2018-08-17 09:51:02 -0700

    Added more stuff to rawDiscord.js

--- server/rawDiscord.js
+++ server/rawDiscord.js
@@ -22,3 +22,36 @@ module.exports.exchangeToken = function(authToken, redirectHost, callback) {
 		callback(body);
 	});
 }
+
+module.exports.getUserData = function(accessToken, callback) {
+	request({
+		url: 'https://discordapp.com/api/users/@me',
+		method: 'GET',
+		headers: {
+			Authorization: 'Bearer ' + accessToken
+		},
+		json: true
+	}, (err, res, body) => {
+		if (err) throw err;
+		callback(body);
+	});
+}
+
+module.exports.getNewToken = function(refreshToken, redirectHost, callback) {
+	request({
+		url: 'https://discordapp.com/api/oauth2/token',
+		method: 'POST',
+		form: { // Endpoint requires form-encoded data.
+			'client_id': CLIENT_ID,
+			'client_secret': CLIENT_SECRET,
+			'grant_type': 'refresh_token',
+			'refresh_token': refreshToken,
+			'redirect_uri': redirectHost + '/api/v1/discord/authCallback',
+			'scope': SCOPES
+		},
+		json: true
+	}, (err, res, body) => {
+		if (err) throw err;
+		callback(body);
+	})
+}
