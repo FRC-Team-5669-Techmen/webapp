0f68ca5b47df20e8bc3caca0a0bcc9381857149c
CodeCube <therealcodecube@gmail.com>
2018-09-03 19:24:11 -0700

    Made URI fix bug thing

--- src/app/discord.service.ts
+++ src/app/discord.service.ts
@@ -2,7 +2,8 @@ import { WebappBackendService } from './webapp-backend.service';
 import { Injectable } from '@angular/core';
 
 const DISCORD_CLIENT_ID = '478633380267950090';
-const REDIRECT_URI = 'http://localhost:4200/api/v1/discord/authCallback';
+const REDIRECT_URI = (global['productionMode'] ? 'https://frc5669.port0.org' : 'http://localhost:4200') + '/api/v1/discord/authCallback';
+console.log(REDIRECT_URI);
 const AUTH_URI = `https://discordapp.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}`
   + `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify%20email%20guilds%20guilds.join`;
 
