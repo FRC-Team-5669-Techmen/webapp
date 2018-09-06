194743d1b4f3fc801adc2ccacb7ee5c5a385888f
CodeCube <therealcodecube@gmail.com>
2018-09-05 17:14:20 -0700

    Fixed a very stupid bug

--- src/app/discord.service.ts
+++ src/app/discord.service.ts
@@ -2,8 +2,7 @@ import { WebappBackendService } from './webapp-backend.service';
 import { Injectable } from '@angular/core';
 
 const DISCORD_CLIENT_ID = '478633380267950090';
-const REDIRECT_URI = (global['productionMode'] ? 'https://frc5669.port0.org' : 'http://localhost:4200') + '/api/v1/discord/authCallback';
-console.log(REDIRECT_URI);
+const REDIRECT_URI = 'https://frc5669.port0.org' + '/api/v1/discord/authCallback';
 const AUTH_URI = `https://discordapp.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}`
   + `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify%20email%20guilds%20guilds.join`;
 
